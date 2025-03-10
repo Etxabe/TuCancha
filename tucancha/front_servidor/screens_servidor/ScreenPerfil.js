4c985a07cb92b913d288d54ab30edbb41729afac
Ander Etxabe <etxabin@gmail.com>
2025-03-10 12:44:08 +0100

    Arreglos Etxabe

--- tucancha/front_servidor/screens_servidor/ScreenPerfil.js
+++ tucancha/front_servidor/screens_servidor/ScreenPerfil.js
@@ -1,17 +1,23 @@
 import { createNativeStackNavigation} from '@react-navigation/stack'
 
-import { StyleSheet, View,Button,Input, Dimensions}from 'react-native';
+import { StyleSheet, View,Button,Input, Dimensions, Image, Text, TouchableOpacity}from 'react-native';
 import React, { useState } from "react";
+import Icon from 'react-native-vector-icons/Feather'; // Importamos el ícono
 
 
 const { width, height } = Dimensions.get("window");
 
 
-export default function ScreenPerfil() {
+export default function FrontViewPitch1() {
 
   return (
       <View style={styles.container}>
-        <Button title='hola2' ></Button>
+        <Image source={require('./../../assets/logo-TuCancha.png')} style={styles.image} />
+        <Text style={styles.nombre}>Tucán Cha</Text>
+        <Text style={styles.descripcion}>¡El Tucán más deportista!</Text>
+        <TouchableOpacity style={styles.editButton} onPress={() => alert('Editar perfil')}>
+          <Icon name="edit" size={24} color="#000" />
+        </TouchableOpacity>
       </View>
   );
 }
@@ -23,8 +29,31 @@ const styles = StyleSheet.create({
       height: height,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       borderWidth: 1,
-      padding: height*0.07
+      padding: height*0.07,
+      paddingTop: height * 0.15, // Aumenta el espacio superior
+    },
+    image: {
+      width: 175,
+      height: 175,
+      borderRadius: 40,
+      borderWidth: 1,
+    borderColor: 'black',
+    },
+    nombre: {
+      marginTop: 25,      // Espacio entre la imagen y el nombre
+      fontSize: 20,       // Tamaño del texto
+      fontWeight: 'bold', // Negrita
+      color: '#333',      // Color oscuro para mejor visibilidad
+    },
+    descripcion: {
+      marginTop: 15,      // Espacio entre la imagen y el nombre
+      fontSize: 16,       // Tamaño del texto
+      color: '#333',      // Color oscuro para mejor visibilidad
+    },
+    editButton: {
+      marginTop: 10, // Espacio debajo del nombre
+      padding: 10,   // Área táctil más grande
     },
 });  
