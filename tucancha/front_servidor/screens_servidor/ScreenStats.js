010211a80cc3ef89a8659948cd95f33c1f092bfc
aritzhu <aritzhu95@gmail.com>
2025-05-05 12:09:17 +0200

    graficos para lso proveedores

---
+++ tucancha/front_servidor/screens_servidor/ScreenStats.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReservasGraficas from '../../backend/funciones_backend/NumeroReservasProvider';
+
+export default function ScreenListadoInstalaciones() {
+  return <ReservasGraficas />;
+}
